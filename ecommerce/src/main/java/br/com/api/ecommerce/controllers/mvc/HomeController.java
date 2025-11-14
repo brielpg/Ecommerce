22122ecd48666353bc6498b82f5303495a52ce1b
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-11-14 20:27:42 -0300

    feat: transparency mechanics for favorite products

--- ecommerce/src/main/java/br/com/api/ecommerce/controllers/mvc/HomeController.java
+++ ecommerce/src/main/java/br/com/api/ecommerce/controllers/mvc/HomeController.java
@@ -1,16 +1,22 @@
 package br.com.api.ecommerce.controllers.mvc;
 
+import br.com.api.ecommerce.models.User;
 import br.com.api.ecommerce.models.dtos.Category.CategoryDtoList;
 import br.com.api.ecommerce.models.dtos.Product.ProductDtoList;
 import br.com.api.ecommerce.services.CategoryService;
 import br.com.api.ecommerce.services.ProductService;
+import br.com.api.ecommerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -21,11 +27,20 @@ public class HomeController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(@AuthenticationPrincipal User user, Model model) {
         Pageable pageable = PageRequest.of(0, 10); // Página 0, tamanho 10
         Page<ProductDtoList> products = productService.getAll(pageable);
         Page<CategoryDtoList> categories = categoryService.getAll(pageable);
+
+        if (user != null) {
+            List<ProductDtoList> favorites = userService.getFavorites(user.getId());
+            List<UUID> favoriteIds = favorites.stream().map(ProductDtoList::id).toList();
+            model.addAttribute("favoriteIds", favoriteIds);
+        }
 
         model.addAttribute("products", products);
         model.addAttribute("categories", categories);
