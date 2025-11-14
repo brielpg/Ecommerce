1f730525a5aff00ea2f4da35ccfc9aa8b9c3a6e9
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-11-14 19:46:43 -0300

    feat: updating error page

--- ecommerce/src/main/java/br/com/api/ecommerce/config/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/br/com/api/ecommerce/config/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -60,9 +61,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErrorDto> handleGeneralException(Exception ex) {
-        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorDto errorDto = new ErrorDto(httpStatus.value(), httpStatus.name(), List.of(ex.getMessage()));
-        return ResponseEntity.status(httpStatus).body(errorDto);
+    public String handleGeneralException(Exception ex, Model model) {
+//        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
     }
 }
