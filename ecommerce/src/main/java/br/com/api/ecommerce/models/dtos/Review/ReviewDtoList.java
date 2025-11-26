5c1bc20adf298795039413e4d2e180151bc931a0
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-11-26 02:04:22 -0300

    feat: change ReviewDtoList fields user & product to -> userId & productId

--- ecommerce/src/main/java/br/com/api/ecommerce/models/dtos/Review/ReviewDtoList.java
+++ ecommerce/src/main/java/br/com/api/ecommerce/models/dtos/Review/ReviewDtoList.java
@@ -1,14 +1,12 @@
 package br.com.api.ecommerce.models.dtos.Review;
 
-import br.com.api.ecommerce.models.dtos.Product.ProductDtoList;
-import br.com.api.ecommerce.models.dtos.User.UserDtoList;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record ReviewDtoList(
-        UserDtoList user,
-        ProductDtoList product,
+        UUID id,
+        UUID userId,
+        UUID productId,
         Double rating,
         String review,
         LocalDate timestamp
