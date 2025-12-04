ed8a7764868105cec02ce3b0d27d4c7aef1bc9e5
Gabriel Galiza <briel.galiza@gmail.com>
2025-12-04 18:21:43 -0300

    feat: application configured to enable cache

---
+++ ecommerce/src/main/java/br/com/api/ecommerce/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package br.com.api.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${api.redis.timeout}")
+    private Integer redisTimeout;
+
+    @Bean
+    public RedisCacheConfiguration defaultCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(redisTimeout))
+                .disableCachingNullValues()
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
