9461466e7205d98143851614323482fec603b247
Gabriel Galiza <briel.galiza@gmail.com>
2025-12-08 22:05:00 -0300

    feat: added new exceptions, updated PreAuthorize annotations, and added new methods to ReviewService

--- ecommerce/src/main/java/br/com/api/ecommerce/services/ReviewService.java
+++ ecommerce/src/main/java/br/com/api/ecommerce/services/ReviewService.java
@@ -2,11 +2,13 @@ package br.com.api.ecommerce.services;
 
 import br.com.api.ecommerce.exceptions.ConflictException;
 import br.com.api.ecommerce.exceptions.NotFoundException;
+import br.com.api.ecommerce.exceptions.AccessDeniedException;
 import br.com.api.ecommerce.models.Product;
 import br.com.api.ecommerce.models.Review;
 import br.com.api.ecommerce.models.dtos.Review.ReviewDtoCreate;
 import br.com.api.ecommerce.models.dtos.Review.ReviewDtoList;
 import br.com.api.ecommerce.models.dtos.Review.ReviewDtoUpdate;
+import br.com.api.ecommerce.models.enums.OrderStatus;
 import br.com.api.ecommerce.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -29,15 +31,19 @@ public class ReviewService {
     private ProductService productService;
 
     @Autowired
+    private OrderService orderService;
+
+    @Autowired
     private UserService userService;
 
-    // TODO adjust PreAuthorizes to support 'authentication.principal.id'
-
     @Transactional
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("#dto.userId() == authentication.principal.id or hasRole('ADMIN')")
     public ReviewDtoList create(ReviewDtoCreate dto){
         this.existsByUserIdAndProductId(dto.userId(), dto.productId());
-        // TODO implement purchase required validation before create new Review
+
+        if (!orderService.existsByUserIdAndProductIdAndStatus(dto.userId(), dto.productId(), OrderStatus.DELIVERED)) {
+            throw new AccessDeniedException("exception.review.purchase.required");
+        }
 
         Review review = dtoToEntity(dto);
 
@@ -60,7 +66,7 @@ public class ReviewService {
     }
 
     @Transactional
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("@reviewService.getUserIdFromReview(#dto.id()) == authentication.principal.id or hasRole('ADMIN')")
     public ReviewDtoList update(ReviewDtoUpdate dto) {
         Review review = this.getById(dto.id());
 
@@ -75,11 +81,12 @@ public class ReviewService {
     }
 
     @Transactional
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("@reviewService.getUserIdFromReview(#id) == authentication.principal.id or hasRole('ADMIN')")
     public void delete(UUID id) {
-        // TODO fix method error
         Review review = this.getById(id);
+        Product product = review.getProduct();
 
+        product.getReviews().remove(review);
         repository.delete(review);
 
         productService.updateProductRating(review.getProduct().getId());
@@ -89,6 +96,11 @@ public class ReviewService {
     private void existsByUserIdAndProductId(UUID userId, UUID productId){
         if (repository.existsByUserIdAndProductId(userId, productId))
             throw new ConflictException("exception.review.already.exists");
+    }
+
+    @Transactional(readOnly = true)
+    public UUID getUserIdFromReview(UUID reviewId) {
+        return this.getById(reviewId).getUser().getId();
     }
 
     @Transactional
