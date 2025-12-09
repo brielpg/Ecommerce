cad0453b0fc8af2ce2c0d6f08c8dc1addc5ffb86
Gabriel Galiza <briel.galiza@gmail.com>
2025-12-08 21:54:14 -0300

    feat: add check for existing order by user, product, and status

--- ecommerce/src/main/java/br/com/api/ecommerce/repositories/OrderRepository.java
+++ ecommerce/src/main/java/br/com/api/ecommerce/repositories/OrderRepository.java
@@ -1,6 +1,7 @@
 package br.com.api.ecommerce.repositories;
 
 import br.com.api.ecommerce.models.Order;
+import br.com.api.ecommerce.models.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,7 @@ import java.util.UUID;
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     @Query("SELECT o FROM Order o JOIN FETCH o.user u WHERE u.id = :userId AND u.active = true")
     Page<Order> findOrdersByUserId(@Param("userId") UUID userId, Pageable pageable);
+
+    @Query("SELECT COUNT(o) > 0 FROM Order o JOIN o.items i WHERE o.user.id = :userId AND i.product.id = :productId AND o.status = :status")
+    boolean existsByUserIdAndProductIdAndStatus(@Param("userId") UUID userId, @Param("productId") UUID productId, @Param("status") OrderStatus status);
 }
