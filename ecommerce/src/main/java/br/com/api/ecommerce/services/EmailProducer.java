ba3bbb57ea9a7bed129a84cc0cf8184642e604e4
Gabriel Galiza <briel.galiza@gmail.com>
2025-12-27 01:35:12 -0300

    feat: created EmailProducer

---
+++ ecommerce/src/main/java/br/com/api/ecommerce/services/EmailProducer.java
@@ -0,0 +1,27 @@
+package br.com.api.ecommerce.services;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class EmailProducer {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${email.queue.name}")
+    private String queueName;
+
+    public void publishEvent(String eventType, Map<String, Object> data) {
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("eventType", eventType);
+        payload.put("data", data);
+
+        rabbitTemplate.convertAndSend(queueName, payload);
+    }
+}
